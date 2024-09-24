9dfa23c16643f813bbcc8ebe571da7e72ee95113
devjaveriasohail <javeriasohail996@gmail.com>
2024-09-24 10:06:50 +0500

    logical Operator

---
+++ 7.logical.py
@@ -0,0 +1,8 @@
+# or  -give us true answer when both values are true and either one of the value is true
+print(3>8 or 5>4) #give true answer becz 2nd condition is true
+
+# and  -give us true answer only when both values are true and false even one of the value is true
+print(3>8 and 5>4) #give false answer becz 1nd condition is false
+
+# not - it reverse the given answer change True to false and False to true
+print(not 5>4) # answer is true but not operator reverse it to false
