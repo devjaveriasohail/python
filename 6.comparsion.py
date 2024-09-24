875d9b320051a359fce1802f04154391a85a4642
devjaveriasohail <javeriasohail996@gmail.com>
2024-09-24 09:43:44 +0500

    Comparsion Operator

---
+++ 6.comparsion.py
@@ -0,0 +1,18 @@
+# comparsion operator return the answer
+# greater than
+print("greater =",5>4)
+
+# less than
+print("lesser=",5<4)
+
+#greater than equal to 
+print("greater than equal to =",5>=5)
+
+#less than equal to 
+print("lesser than equal to =",5<=4)
+
+# equal to
+print("equal to =",5==4)
+
+# not equal to
+print("not equal to =",5!=4)
