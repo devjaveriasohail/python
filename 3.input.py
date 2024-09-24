f9dd221253c52041f808ede7a2037af16e7a14f1
devjaveriasohail <javeriasohail996@gmail.com>
2024-09-24 08:31:09 +0500

    input

--- 3.input.py
+++ 3.input.py
@@ -6,3 +6,17 @@ print("Hi ", name)
 
 age=input("Enter your age ")
 print("your age is ",age)
+
+"""" all the inputed store in the string data type 
+ to chnage this we use casting- where we change its data type using 
+int( ) convert to integer datatype
+float() convert to float datatype
+bool()  convert to bool datatype
+str()  convert to str datatype
+"""
+print(type(name))
+print(type(age))
+
+# casting the age variable datatype to int
+new_age=int(age)
+print("after casting the data type of age variable is ", type(new_age))
